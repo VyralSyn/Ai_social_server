87fbd469ca20dc7bbc40e5ef4f6b21a5da22d503
Vyralsyn <Vyralsyn@proton.me>
2025-03-01 05:49:30 +1300

    Initial commit

---
+++ users/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets, serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
